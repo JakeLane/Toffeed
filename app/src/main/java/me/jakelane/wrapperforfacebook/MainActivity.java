6c17c0d2aef7e85dac7526e3df989b0491769021
Jake Lane <me@jakelane.me>
2015-11-26 15:27:34 +1030

    Fixed vector compatibility

--- app/src/main/java/me/jakelane/wrapperforfacebook/MainActivity.java
+++ app/src/main/java/me/jakelane/wrapperforfacebook/MainActivity.java
@@ -31,6 +31,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
