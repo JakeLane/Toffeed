5d3e8d68697021d023dfc33742f867a1e3e6d4b3
Jake Lane <me@jakelane.me>
2015-11-27 14:11:11 +1030

    Changed to switch statement and added settings button

--- app/src/main/java/me/jakelane/wrapperforfacebook/MainActivity.java
+++ app/src/main/java/me/jakelane/wrapperforfacebook/MainActivity.java
@@ -108,29 +108,41 @@ public class MainActivity extends AppCompatActivity
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_news) {
-            wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#feed_jewel > a').click();})();");
-            item.setChecked(true);
-        } else if (id == R.id.nav_friendreq) {
-            wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#requests_jewel > a').click();})();");
-            item.setChecked(true);
-        } else if (id == R.id.nav_messages) {
-            wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#messages_jewel > a').click();})();");
-            item.setChecked(true);
-        } else if (id == R.id.nav_search) {
-            wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#search_jewel > a').click();})();");
-            item.setChecked(true);
-        } else if (id == R.id.nav_mainmenu) {
-            wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#bookmarks_jewel > a').click();})();");
-            item.setChecked(true);
-        } else if (id == R.id.nav_back) {
-            wrapperWebView.goBack();
-        } else if (id == R.id.nav_reload) {
-            wrapperWebView.reload();
-        } else if (id == R.id.nav_forward) {
-            wrapperWebView.goForward();
+        switch (item.getItemId()) {
+            case R.id.nav_news:
+                wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#feed_jewel > a').click();})();");
+                item.setChecked(true);
+                break;
+            case R.id.nav_friendreq:
+                wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#requests_jewel > a').click();})();");
+                item.setChecked(true);
+                break;
+            case R.id.nav_messages:
+                wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#messages_jewel > a').click();})();");
+                item.setChecked(true);
+                break;
+            case R.id.nav_search:
+                wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#search_jewel > a').click();})();");
+                item.setChecked(true);
+                break;
+            case R.id.nav_mainmenu:
+                wrapperWebView.loadUrl("javascript:(function(){document.querySelector('#bookmarks_jewel > a').click();})();");
+                item.setChecked(true);
+                break;
+            case R.id.nav_back:
+                wrapperWebView.goBack();
+                break;
+            case R.id.nav_reload:
+                wrapperWebView.reload();
+                break;
+            case R.id.nav_forward:
+                wrapperWebView.goForward();
+                break;
+            case R.id.nav_settings:
+                // TODO
+                break;
+            default:
+                break;
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
